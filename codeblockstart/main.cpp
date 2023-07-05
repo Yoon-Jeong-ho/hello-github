45ef2caf44c50d7e7a40b8a334c3802e66b6547b
K\aa007 <aa007878@naver.com>
2023-07-05 10:27:13 +0900

    저기 안되니까 여기로 대피다...

---
+++ codeblockstart/main.cpp
@@ -0,0 +1,15 @@
+#include<fstream>
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<Eigen/Dense>
+#include <unsupported/Eigen/CXX11/Tensor>
+
+using namespace Eigen;
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
