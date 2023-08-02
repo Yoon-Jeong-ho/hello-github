c34f3b955e293f6b6e831d2aebfc7b65e4c10e88
K\aa007 <aa007878@naver.com>
2023-08-02 20:37:17 +0900

    행렬에 대해 잘 생각해보기

---
+++ 연구실 자료/coefvisualization.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3205d51313da9bdc73978f6098537ace548f5d3276ac15273407de37934ebf4a
+size 10850
---
+++ 연구실 자료/makesomething/main.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8309fb886ccce87dca508435df085541da6acdfc9765637d6edc1e9ea6efb1cd
+size 9624
